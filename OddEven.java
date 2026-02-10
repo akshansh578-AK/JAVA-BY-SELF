de6441f2646908a8f06619c7923c521f19586596
Akshansh Mishra <akshansh578@gmail.com>
2026-02-10 15:01:58 +0530

    DAY - 02 (CONDITIONAL-STATEMENTS)

---
+++ OddEven.java
@@ -0,0 +1,17 @@
+package CONDITIONAL_STATEMENTS;
+
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args){
+        System.out.print("Enter the number: ");
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        if(A%2==0){
+            System.out.println("THE NUMBER IS EVEN");
+        }
+        else{
+            System.out.print("THE NUMBER IS ODD");
+        }
+    }
+}
